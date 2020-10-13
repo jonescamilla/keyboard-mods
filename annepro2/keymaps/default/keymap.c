52ddc76593d6f8142148a31af93382fcc71e5f3b
jon.escamilla <jonathanescamilla1@gmail.com>
2020-10-13 14:17:29 -0700

    updated keymap.c for qmk

--- annepro2/keymaps/default/keymap.c
+++ annepro2/keymaps/default/keymap.c
@@ -8,6 +8,8 @@ enum anne_pro_layers {
   _MO_2,
 };
 
+const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
  /* Layer _BASE_LAYER
   * ,-----------------------------------------------------------------------------------------.
   * | esc |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |  -  |  =  |   Bksp    |
@@ -22,14 +24,13 @@ enum anne_pro_layers {
   * \-----------------------------------------------------------------------------------------/
   */
 
- const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
- [_BASE_LAYER] = KEYMAP( /* Base */
+  [_BASE_LAYER] = KEYMAP( /* Base */
     KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC,
     KC_TAB, KC_Q, KC_W, KC_F, KC_P, KC_B, KC_J, KC_L, KC_U, KC_Y, KC_SCLN, KC_LBRC, KC_RBRC, KC_BSLS,
     MO(_MO_1), KC_A, KC_R, KC_S, KC_T, KC_G, KC_K, KC_N, KC_E, KC_I, KC_O, KC_QUOT, KC_ENT,
     KC_LSFT, KC_X, KC_C, KC_D, KC_V, KC_Z, KC_M, KC_H, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
     KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, MO(_MO_2), KC_LEFT, KC_DOWN, KC_RGHT
-),
+  ),
 
  /* Layer _MO_1
   * ,-----------------------------------------------------------------------------------------.
@@ -45,13 +46,13 @@ enum anne_pro_layers {
   * \-----------------------------------------------------------------------------------------/
   */
 
- [_MO_1] = KEYMAP(
+  [_MO_1] = KEYMAP(
     KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_TRNS, 
     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_UP, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
     KC_TRNS, KC_LSFT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_LEFT, KC_DOWN, KC_RGHT, KC_MPLY, KC_GRV, KC_TRNS,
     KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_UP,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_LALT, MO(_MO_2), KC_LEFT, KC_DOWN, KC_RGHT
-),
+  ),
 
  /* Layer _MO_2
   * ,-----------------------------------------------------------------------------------------.
@@ -67,13 +68,13 @@ enum anne_pro_layers {
   * \-----------------------------------------------------------------------------------------/
   */
 
- [_MO_2] = KEYMAP( /* Base */
+  [_MO_2] = KEYMAP( /* Base */
     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_VOLD, KC_VOLU, KC_NO,
     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_MRWD, KC_MPLY, KC_MFFD, KC_NO,
     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_UP,
     KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT
- ),
+  ),
 };
 
 const uint16_t keymaps_size = sizeof(keymaps);
