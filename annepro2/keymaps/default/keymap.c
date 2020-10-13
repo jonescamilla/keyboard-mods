99755ba78963134e33884bfbf6cef0abacd84aa2
jon.escamilla <jonathanescamilla1@gmail.com>
2020-10-13 14:04:54 -0700

    updated keymap.c for qmk

--- annepro2/keymaps/default/keymap.c
+++ annepro2/keymaps/default/keymap.c
@@ -4,13 +4,13 @@
 
 enum anne_pro_layers {
   _BASE_LAYER,
-  _FN1_LAYER,
-  _FN2_LAYER,
+  _MO_1,
+  _MO_2,
 };
 
  /* Layer _BASE_LAYER
   * ,-----------------------------------------------------------------------------------------.
-  * | esc |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |  -  |  =  |    Bksp   |
+  * | esc |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |  -  |  =  |   Bksp    |
   * |-----------------------------------------------------------------------------------------+
   * | Tab    |  q  |  w  |  f  |  p  |  b  |  j  |  l  |  u  |  y  |  ;  |  [  |  ]  |   \    |
   * |-----------------------------------------------------------------------------------------+
@@ -26,14 +26,14 @@ enum anne_pro_layers {
  [_BASE_LAYER] = KEYMAP( /* Base */
     KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC,
     KC_TAB, KC_Q, KC_W, KC_F, KC_P, KC_B, KC_J, KC_L, KC_U, KC_Y, KC_SCLN, KC_LBRC, KC_RBRC, KC_BSLS,
-    MO(_FN1_LAYER), KC_A, KC_R, KC_S, KC_T, KC_G, KC_K, KC_N, KC_E, KC_I, KC_O, KC_QUOT, KC_ENT,
+    MO(_MO_1), KC_A, KC_R, KC_S, KC_T, KC_G, KC_K, KC_N, KC_E, KC_I, KC_O, KC_QUOT, KC_ENT,
     KC_LSFT, KC_X, KC_C, KC_D, KC_V, KC_Z, KC_M, KC_H, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-    KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, MO(_FN2_LAYER), KC_LEFT, KC_DOWN, KC_RGHT
+    KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, MO(_MO_2), KC_LEFT, KC_DOWN, KC_RGHT
 ),
 
- /* Layer _FN1_LAYER
+ /* Layer _MO_1
   * ,-----------------------------------------------------------------------------------------.
-  * |  ~  |  F1 |  F2 |  F3 |  F4 |  F5 |  F6 |  F7 |  F8 |  F9 | F10 | F11 | F12 |  DELETE   |
+  * |  ~  |  F1 |  F2 |  F3 |  F4 |  F5 |  F6 |  F7 |  F8 |  F9 | F10 | F11 | F12 |   Bksp    |
   * |-----------------------------------------------------------------------------------------+
   * |        |     |     |     |     |     |     |     | Up  |     |     |     |     |        |
   * |-----------------------------------------------------------------------------------------+
@@ -45,15 +45,15 @@ enum anne_pro_layers {
   * \-----------------------------------------------------------------------------------------/
   */
 
- [_FN1_LAYER] = KEYMAP( /* Base */
+ [_MO_1] = KEYMAP(
     KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_TRNS, 
     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_UP, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
     KC_TRNS, KC_LSFT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_LEFT, KC_DOWN, KC_RGHT, KC_MPLY, KC_GRV, KC_TRNS,
     KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_UP,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_LALT, MO(_FN2_LAYER), KC_LEFT, KC_DOWN, KC_RGHT
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_LALT, MO(_MO_2), KC_LEFT, KC_DOWN, KC_RGHT
 ),
 
- /* Layer _FN2_LAYER
+ /* Layer _MO_2
   * ,-----------------------------------------------------------------------------------------.
   * |     |     |     |     |     |     |     |     |     |     |     |Vol- |Vol+ |           |
   * |-----------------------------------------------------------------------------------------+
@@ -67,7 +67,7 @@ enum anne_pro_layers {
   * \-----------------------------------------------------------------------------------------/
   */
 
- [_FN2_LAYER] = KEYMAP( /* Base */
+ [_MO_2] = KEYMAP( /* Base */
     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_VOLD, KC_VOLU, KC_NO,
     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_MRWD, KC_MPLY, KC_MFFD, KC_NO,
