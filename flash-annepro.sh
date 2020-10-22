d212cbbd12e11a97e1d33b279296baa77512b97c
jon.escamilla <jonathanescamilla1@gmail.com>
2020-10-22 15:41:32 -0700

    adjusted psuedo code

--- flash-annepro.sh
+++ flash-annepro.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# as soon as you run this script you can disconnect your keyboard and connect it in DFU/IAP mode
+# there will be no issue with the keyboard not being detected
+
 # move file from git to documents
 cp /Users/admin/git/keyboard-mods/annepro2/keymaps/default/keymap.c /Users/admin/Documents/annepro/annepro-qmk/keyboards/annepro2/keymaps/default
 
