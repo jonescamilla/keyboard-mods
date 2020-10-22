bf6fa957bde43e2076c0b10a51a4c7524e7c1580
jon.escamilla <jonathanescamilla1@gmail.com>
2020-10-22 15:37:15 -0700

    adjusted macros

--- flash-annepro.sh
+++ flash-annepro.sh
@@ -10,10 +10,10 @@ make annepro2/c18
 
 # move the built default to annepro tools
 echo ""
-mv /Users/admin/Documents/annepro/annepro-qmk/annepro2_c18_default.bin /Users/admin/Documents/annepro/AnnePro2-Tools-d10d476b42df2cbacdc52671b1c41ef0ac0b91b7/target/release/annepro2_c18_default.bin
+mv /Users/admin/Documents/annepro/annepro-qmk/annepro2_c18_default.bin /Users/admin/Documents/annepro/AnnePro2-Tools/target/release/annepro2_c18_default.bin
 
 # navigating to flash...
-cd /Users/admin/Documents/annepro/AnnePro2-Tools-d10d476b42df2cbacdc52671b1c41ef0ac0b91b7/target/release/
+cd /Users/admin/Documents/annepro/AnnePro2-Tools/target/release/
 
 # flash annepro
 echo ""
